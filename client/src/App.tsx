c2f8a034bbdc735b7abfe5e22fcb254d2a62d8dc
JamesDeChavez <jamesdechavez23@gmail.com>
2023-07-08 17:46:23 -0700

    fixed verify error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -53,7 +53,6 @@ function App() {
     Auth.currentAuthenticatedUser()
       .then(currentUser => {
         setUserData(currentUser)
-        setUserLoggedIn(true)
       })
       .catch(() => {
         return
