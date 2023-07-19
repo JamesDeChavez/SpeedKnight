b115cc50d683f9674a614a2d9a7ef51ba3b95b78
JamesDeChavez <jamesdechavez23@gmail.com>
2023-07-19 13:50:26 -0700

    removed unnecessary import

--- client/src/components/GameOptions/index.tsx
+++ client/src/components/GameOptions/index.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import './styles.css'
 import GlobalContext from '../../utils/GlobalContext'
 
