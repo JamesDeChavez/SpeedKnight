a8aa972355e4ed33d1affe19f6e8a8eb7e0d7d01
JamesDeChavez <jamesdechavez23@gmail.com>
2023-07-21 23:47:41 -0700

    fixed warning for deploy

--- client/src/tests/createEmptyBoard.test.tsx
+++ client/src/tests/createEmptyBoard.test.tsx
@@ -3,6 +3,7 @@ import { createEmptyBoard } from "../game/functions";
 describe("createEmptyBoard", () => {
     it("should create an empty board", () => {
         const board = createEmptyBoard()
+        console.log(board)
         // expect(board).toEqual([
         //     [0, 0, 0, 0, 0, 0, 0, 0, 0],
         //     [0, 0, 0, 0, 0, 0, 0, 0, 0],
